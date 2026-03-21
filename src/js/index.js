b45c1c765fd6bfa210c7b2226bd3af0937883f6a
a.puchkina <anna.puchkina.04>
2026-03-22 01:09:09 +0400

    адаптивность

--- src/js/index.js
+++ src/js/index.js
@@ -5,4 +5,17 @@ import initTestimonialsSlider from "./modules/testimonials-slider";
 
 window.addEventListener("load", () => {
     initTestimonialsSlider();
+
+    const burger = document.querySelector(".header__burger");
+    const headerRight = document.querySelector(".header__right");
+
+    if (burger && headerRight) {
+        burger.addEventListener("click", () => {
+            burger.classList.toggle("is-active");
+            headerRight.classList.toggle("is-open");
+
+            const isExpanded = burger.classList.contains("is-active");
+            burger.setAttribute("aria-expanded", isExpanded ? "true" : "false");
+        });
+    }
 });
